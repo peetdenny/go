ad8e9b42f23a97b056322b3e8c1e813d1b46cb06
denny <peetdenny@gmail.com>
2015-11-19 22:35:30 +0000

    first working test

--- astro/rocketry/rocket_calcs.go
+++ astro/rocketry/rocket_calcs.go
@@ -1,9 +1,11 @@
 package rocketry 
 import (
-	"fmt";
+	"math";
 )
 
+const grabbity = 9.81
 
-func calc_delta_v(impulse float32){
-	fmt.Println(impulse)
+// dv = Gravity * impulse * natural log of (starting mass / end mass)
+func Calc_delta_v(mass1 float64, mass2 float64, impulse float64 ) float64{
+	return grabbity * impulse * math.Log(mass1/mass2)
 }
--- astro/rocketry/rocket_calcs_test.go
+++ astro/rocketry/rocket_calcs_test.go
@@ -5,7 +5,24 @@ import(
 	"testing";
 )
 
-func TestIdealRocketEquation(t *testing.T){
-	fmt.Println("Test run")
+func fuzzy_match(l float64, r float64) bool{
+	//The canonical use of math/big.NewFloat doesn't seem to be available in this version. Hence rolling my own dubious comparator, to compare to one decimal place
+	L := int32((l*10))
+	R := int32((r*10))
+	return L == R
+}
 
+func TestIdealRocketEquation(t *testing.T){
+	fmt.Println("Testing")
+	mass1 := 2400.0
+	mass2 := 400.0
+	specificImpulse := 210.0
+	dv := Calc_delta_v(mass1, mass2, specificImpulse)	
+	if ! fuzzy_match(dv, 3691.2) {
+		t.Error("For", mass1, mass2, "and", specificImpulse,
+			"Expected", 3691.2,
+			"Instead, received", dv,
+
+		)
+	}
 }
